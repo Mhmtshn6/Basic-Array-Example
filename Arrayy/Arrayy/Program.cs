948d6cbcf29848aa25ffc501d033ef5cfa6fc1a5
Mhmtshn6 <mehmet68064@gmail.com>
2024-03-17 02:23:58 +0300

    Basic-Array-Example
    Simple example of Array, one of the linear data structures

---
+++ Arrayy/Arrayy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Software = new string[4];
+
+            Software[0] = "C#";
+            Software[1] = "C++";
+            Software[2] = "Java";
+            Software[3] = "Phyton";
+
+            for (int i = 0; i < Software.Length; i++)
+            {
+                Console.WriteLine(Software[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
